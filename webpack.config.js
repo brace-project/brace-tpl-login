aeb8a676e6d172c66c6c353b21c6b4cb1617fbac
Matthias Leuffen <matthes@leuffen.de>
2024-03-31 13:29:59 +0200

    sync

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+﻿const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        'src/tpl/login/index.ts': 'src/tpl/login/index_gen.js'
+    },
+    cache: true,
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'esbuild-loader',
+                        options: {
+                            loader: "ts",
+                        }
+                    }
+                ],
+
+                include: [
+                    path.resolve(__dirname, "src"),
+                    path.resolve(__dirname, "workspaces")
+                ]
+            },
+            {
+                enforce: 'pre',
+                test: /\.html$/,
+                loader: 'raw-loader',
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+
+                        loader: 'css-loader',
+                        options: {
+                            url: false // don't complain about url() in css
+                        }
+
+                    }, 'sass-loader'],
+                include: path.resolve(__dirname, "")
+            },
+
+
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devtool: 'source-map',
+    mode: "development",
+    //mode: "production",
+
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '/'),
+    },
+};
